e5ff346965da03168042d2fb118ce1fb71e215b7
KatanaTakahashi <KatsuroTakahashi05@yandex.ru>
2024-08-29 01:24:24 +0600

    add devops(postgres)

--- src/core/config/local.py
+++ src/core/config/local.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 from .helpers.env_reader import env
@@ -11,7 +13,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
--- src/core/config/prod.py
+++ src/core/config/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 
 from .helpers.env_reader import env, csv
@@ -12,7 +14,12 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS', cast=csv())
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
     }
 }
+

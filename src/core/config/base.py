dba4e7dc29b6eaeb865203c14bed141209c928d0
KatanaTakahashi <KatsuroTakahashi05@yandex.ru>
2024-08-29 20:38:25 +0600

    add custiom tag amd resolve bug for version django==5.0.1

--- src/core/config/base.py
+++ src/core/config/base.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
             ],
+            "builtins": ["about.tags"],
         },
     },
 ]

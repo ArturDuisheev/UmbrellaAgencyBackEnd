382a2d664ff968f22dbc1c7249542ae7110fac50
KatanaTakahashi <KatsuroTakahashi05@yandex.ru>
2024-09-02 00:29:49 +0600

    add media

--- src/core/config/base.py
+++ src/core/config/base.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 from .helpers.jazzmin import *
